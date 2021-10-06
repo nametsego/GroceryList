e3aa6ecde19830cd6c19d77e340b0869ab19ba12
Nametsego <Nametsegothakadu@gmail.com>
2021-10-06 03:51:13 +0200

    Added a list of food items inside of a 2D array list and Titled it MunchiesList

---
+++ src/AntiElite/com/Main.java
@@ -0,0 +1,38 @@
+package AntiElite.com;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<String>> munchiesList = new ArrayList<>();
+
+        ArrayList<String> snackList = new ArrayList<>();
+        snackList.add("Cheetos");
+        snackList.add("Doritos");
+        snackList.add("Skittles");
+
+        ArrayList<String> bevList = new ArrayList<>();
+        bevList.add("Sprite zero sugar");
+        bevList.add("Water");
+        bevList.add("Ginger ale");
+        bevList.add("Lean");
+
+        ArrayList<String> bakeryList = new ArrayList<>();
+        bakeryList.add("Garlic bread");
+        bakeryList.add("Donuts");
+        bakeryList.add("pasta");
+        bakeryList.add("Eggs");
+        bakeryList.add("Lettuce");
+        bakeryList.add("Mushrooms");
+
+        munchiesList.add(bevList);
+        munchiesList.add(snackList);
+        munchiesList.add(bakeryList);
+
+
+
+
+
+    }
+}
